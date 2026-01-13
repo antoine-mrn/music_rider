2bfef02ba4993f4306b0213069beda1a9b18aa4c
antoine-mrn <antoine.marion.am@outlook.fr>
2026-01-13 13:33:12 +0100

    WIP - Signin page

---
+++ frontend/src/components/form/Label.tsx
@@ -0,0 +1,8 @@
+interface LabelProps {
+    label: string;
+    htmlFor: string;
+}
+
+export default function Label({ label, htmlFor }: LabelProps) {
+    return <label htmlFor={htmlFor}>{label}</label>;
+}
--- frontend/src/pages/auth/Signin.tsx
+++ frontend/src/pages/auth/Signin.tsx
@@ -1,3 +1,35 @@
+import { Link } from "react-router";
+import Label from "../../components/form/Label";
+import Input from "../../components/form/Input";
+import Field from "../../components/form/Field";
+
 export default function Signin() {
-    return <div>Signin</div>;
+    return (
+        <div className="flex">
+            <section className="w-full lg:w-1/2 flex flex-col place-items-center">
+                <div>
+                    <h1>Bon retour parmi nous.</h1>
+                    <p>
+                        Pas de compte ?{" "}
+                        <Link to="/signup">S'inscrire gratuitement</Link>
+                    </p>
+                </div>
+                <form>
+                    <Field>
+                        <Label label="e-mail" htmlFor="email" />
+                        <Input type="email" placeholder="john.doe@mail.com" />
+                    </Field>
+                    <Field>
+                        <Label label="Mot de passe" htmlFor="password" />
+                        <Input type="password" />
+                    </Field>
+
+                    <button type="submit">Se connecter</button>
+                </form>
+            </section>
+            <section className="hidden place-content-center lg:flex lg:w-1/2">
+                Le backline est prêt
+            </section>
+        </div>
+    );
 }
