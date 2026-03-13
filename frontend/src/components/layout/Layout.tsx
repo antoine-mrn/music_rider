c993bfdaf1c364b847a1150c678cb20fd285fd10
antoine-mrn <antoine.marion.am@outlook.fr>
2026-03-13 15:49:38 +0100

    Nouvelle navigation

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -1,15 +1,63 @@
-import { Outlet } from "react-router";
+import { NavLink, Outlet } from "react-router";
 import Navbar from "../navbar/Navbar";
 import Footer from "./Footer";
+import { ROUTES } from "../../routes";
+
+const NAVLINKS = [
+    { path: ROUTES.PROFILE, label: "Dashboard", end: true },
+    { path: ROUTES.BANDS, label: "Mes groupes", end: false },
+    { path: ROUTES.HOME, label: "Mes riders", end: false },
+];
 
 export default function PublicLayout() {
     return (
-        <div className="min-h-screen flex flex-col">
-            <Navbar />
-            <main className="flex-1 relative">
-                <Outlet />
-            </main>
-            <Footer />
+        <div className="min-h-screen">
+            <div className="drawer">
+                <input
+                    id="nav-drawer"
+                    type="checkbox"
+                    className="drawer-toggle"
+                />
+                <div className="drawer-content flex flex-col">
+                    {/* Navbar */}
+                    <Navbar navLinks={NAVLINKS} />
+                    {/* Page content */}
+                    <main className="flex-1 relative">
+                        <Outlet />
+                    </main>
+                    <Footer />
+                </div>
+                <div className="drawer-side">
+                    <label
+                        htmlFor="nav-drawer"
+                        aria-label="close sidebar"
+                        className="drawer-overlay"
+                    ></label>
+                    <ul className="menu bg-base-200 min-h-full w-80 p-4 pt-16 space-y-4">
+                        {/* Sidebar content */}
+                        {NAVLINKS.map((link, index) => (
+                            <li key={index}>
+                                <NavLink
+                                    to={link.path}
+                                    onClick={() => {
+                                        const navDrawer =
+                                            document.getElementById(
+                                                "nav-drawer",
+                                            ) as HTMLInputElement;
+                                        if (navDrawer)
+                                            navDrawer.checked = false;
+                                    }}
+                                    className={({ isActive }) =>
+                                        `text-2xl font-black italic uppercase tracking-tight ${isActive ? "text-base-content" : "text-base-content/50"}`
+                                    }
+                                >
+                                    {link.label}
+                                </NavLink>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 }
--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,16 +1,14 @@
-import { Link } from "react-router";
+import { Link, NavLink } from "react-router";
 import AvatarIcon from "./AvatarIcon";
 import { useAuthStore } from "../../store/auth.store";
 import AuthButtonsWrapper from "./AuthButtonsWrapper";
 import { ROUTES } from "../../routes";
 
-const NAVLINKS = [
-    { path: ROUTES.PROFILE, label: "Dashboard" },
-    { path: ROUTES.BANDS, label: "Mes groupes" },
-    { path: ROUTES.HOME, label: "Mes riders" },
-];
-
-export default function Navbar() {
+export default function Navbar({
+    navLinks,
+}: {
+    navLinks: { path: string; label: string; end: boolean }[];
+}) {
     const user = useAuthStore((state) => state.user);
 
     return (
@@ -26,6 +24,29 @@ export default function Navbar() {
                             <PanelRightClose />
                         </label>
                     )} */}
+
+                    <div className="flex-none md:hidden">
+                        <label
+                            htmlFor="nav-drawer"
+                            aria-label="open sidebar"
+                            className="btn btn-square btn-ghost"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                className="inline-block h-6 w-6 stroke-current"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h16M4 18h16"
+                                ></path>
+                            </svg>
+                        </label>
+                    </div>
+
                     <Link
                         to={ROUTES.HOME}
                         className="text-2xl font-black tracking-tighter text-primary italic"
@@ -33,16 +54,25 @@ export default function Navbar() {
                         MUSIC<span className="text-base-content">RIDER</span>
                     </Link>
 
-                    <ul className="flex items-center gap-4 ml-16">
-                        {NAVLINKS.map((link, index) => (
-                            <li
-                                key={index}
-                                className="relative font-black uppercase tracking-tight text-base-content/50 cursor-pointer group transition-colors hover:text-base-content"
-                            >
-                                <Link to={link.path}>
-                                    {link.label}
-                                    <span className="absolute bottom-0 left-0 h-0.5 w-0 bg-primary transition-all duration-300 group-hover:w-full" />
-                                </Link>
+                    <ul className="hidden items-center gap-4 md:flex md:ml-16 md:gap-8">
+                        {navLinks.map((link, index) => (
+                            <li key={index}>
+                                <NavLink
+                                    end={link.end}
+                                    to={link.path}
+                                    className={({ isActive }) =>
+                                        `relative font-black uppercase tracking-tight cursor-pointer group transition-colors hover:text-base-content ${isActive ? "text-base-content" : "text-base-content/50"}`
+                                    }
+                                >
+                                    {({ isActive }) => (
+                                        <>
+                                            {link.label}
+                                            <span
+                                                className={`absolute bottom-0 left-0 h-0.5 w-0 bg-primary transition-all duration-300 group-hover:w-full ${isActive ? "w-full" : "w-0"}`}
+                                            />
+                                        </>
+                                    )}
+                                </NavLink>
                             </li>
                         ))}
                     </ul>
