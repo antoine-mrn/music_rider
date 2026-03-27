8879e89d880ff409d6734ac99507edbf588c8e7b
antoine-mrn <antoine.marion.am@outlook.fr>
2026-03-27 14:45:10 +0100

    Invalidation de toutes les queries après logout

--- frontend/src/features/auth/hooks/useLogout.ts
+++ frontend/src/features/auth/hooks/useLogout.ts
@@ -1,16 +1,18 @@
 import { useNavigate } from "react-router";
 import { useAuthStore } from "../../../store/auth.store";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { authApi } from "../api/auth.api";
 
 export const useLogout = () => {
     const navigate = useNavigate();
     const setLogout = useAuthStore((state) => state.setLogout);
+    const queryClient = useQueryClient();
 
     return useMutation<void, Error, void>({
         mutationFn: () => authApi.logout(),
         onSuccess: () => {
             setLogout();
+            queryClient.clear();
             navigate("/");
         },
     });
