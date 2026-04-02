55d69c3593e5250f8f356fd02077c69be220b8f4
antoine-mrn <antoine.marion.am@outlook.fr>
2026-04-02 22:36:59 +0200

    Logique d'ajout / suppresion d'instrument

--- frontend/src/features/band/components/AddMemberToBandDialog.tsx
+++ frontend/src/features/band/components/AddMemberToBandDialog.tsx
@@ -14,6 +14,7 @@ import {
     AddMemberToBandSchema,
 } from "../../../schemas/add-member-to-band.schema";
 import { useFindAllInstrument } from "../../instrument/hooks/useFindAllInstrument";
+import { useClickOutside } from "../../../hooks/useClickOutside";
 
 const TABS = [
     { label: "Via un compte", mode: "account" },
@@ -29,27 +30,63 @@ export default function AddMemberToBandDialog({
 }) {
     const [selectedTab, setSelectedTab] = useState(0);
 
+    const instrumentSuggestionRef = useRef<HTMLDivElement>(null);
+    const [isSuggestionOpen, setIsSuggestionOpen] = useState<boolean>(false);
+    useClickOutside(instrumentSuggestionRef, () => setIsSuggestionOpen(false));
+
     const { data: instrumentsList } = useFindAllInstrument();
     const [instrumentInputValue, setInstrumentInputValue] = useState("");
-    const [instrumentsSelected, setInstrumentsSelected] = useState<string[]>(
-        [],
+    const [instrumentsSelected, setInstrumentsSelected] = useState<
+        {
+            id: number;
+            label: string;
+        }[]
+    >([]);
+
+    const suggestedInstruments = instrumentsList?.filter((i) =>
+        i.label.toLowerCase().includes(instrumentInputValue),
     );
 
     function handleAddInstrument(instrument: { id: number; label: string }) {
-        const isAlreadyInList = instrumentsSelected.includes(instrument.label);
+        const isAlreadyInList = instrumentsSelected.find(
+            (i) => i.id === instrument.id,
+        );
 
         if (isAlreadyInList) return;
 
-        setInstrumentsSelected([...instrumentsSelected, instrument.label]);
+        setInstrumentsSelected((prev) => [...prev, instrument]);
         setInstrumentInputValue("");
+
+        const selectedIds = watch("instrumentId");
+        setValue("instrumentId", [...selectedIds, instrument.id]);
+    }
+
+    function handleDeleteInstrument(instrument: { id: number; label: string }) {
+        const newInstrumentsSelectedList = instrumentsSelected.filter(
+            (i) => i.id !== instrument.id,
+        );
+
+        setInstrumentsSelected(newInstrumentsSelectedList);
+        setValue("instrumentId", [
+            ...newInstrumentsSelectedList.map((i) => i.id),
+        ]);
     }
 
     const {
         register,
+        setValue,
         handleSubmit,
+        watch,
         formState: { errors },
     } = useForm<AddMemberToBandType>({
         resolver: zodResolver(AddMemberToBandSchema),
+        defaultValues: { instrumentId: [] },
+    });
+
+    console.log(watch());
+
+    const onSubmit = handleSubmit(async (data) => {
+        console.log("🚀 ~ AddMemberToBandDialog ~ data:", data);
     });
 
     const errorsAccount = errors as FieldErrors<
@@ -88,6 +125,7 @@ export default function AddMemberToBandDialog({
                     <Field>
                         <Label label="Instrument joué" htmlFor="instrument" />
                         <Input
+                            onFocus={() => setIsSuggestionOpen(true)}
                             value={instrumentInputValue}
                             onChange={(e) =>
                                 setInstrumentInputValue(e.target.value)
@@ -96,37 +134,64 @@ export default function AddMemberToBandDialog({
                             id="instrument"
                         />
 
-                        {instrumentInputValue.length > 0 && (
-                            <div className="absolute bg-base-100 rounded-xl shadow top-20 w-full overflow-x-auto p-2 grid grid-flow-col gap-2 grid-rows-2">
-                                {instrumentsList
-                                    ?.filter((i) =>
-                                        i.label
-                                            .toLowerCase()
-                                            .includes(instrumentInputValue),
-                                    )
-                                    .map((instrument) => (
-                                        <span
-                                            onClick={() =>
-                                                handleAddInstrument(instrument)
-                                            }
-                                            className="text-sm font-semibold italic p-2 cursor-pointer rounded-lg transition-colors whitespace-nowrap hover:bg-primary/8"
-                                        >
-                                            {instrument.label}
+                        {isSuggestionOpen &&
+                            instrumentInputValue.length > 0 && (
+                                <div
+                                    ref={instrumentSuggestionRef}
+                                    className="absolute z-10 bg-base-100 rounded-xl shadow top-20 w-full overflow-x-auto p-2 grid grid-flow-col gap-2 grid-rows-2"
+                                >
+                                    {suggestedInstruments &&
+                                    suggestedInstruments.length > 0 ? (
+                                        suggestedInstruments.map(
+                                            (instrument) => (
+                                                <span
+                                                    key={instrument.id}
+                                                    onClick={() =>
+                                                        handleAddInstrument(
+                                                            instrument,
+                                                        )
+                                                    }
+                                                    className="text-sm font-semibold italic p-2 cursor-pointer rounded-lg transition-colors whitespace-nowrap hover:bg-primary/8"
+                                                >
+                                                    {instrument.label}
+                                                </span>
+                                            ),
+                                        )
+                                    ) : (
+                                        <span className="text-sm text-base-content/50 italic flex place-content-center">
+                                            Aucun instrument trouvé...
                                         </span>
-                                    ))}
-                            </div>
+                                    )}
+                                </div>
+                            )}
+                        <p className="text-xs text-base-content/50 italic">
+                            Vous pouvez sélectionner un ou plusieurs instruments
+                        </p>
+                        {errors.instrumentId && (
+                            <p className="mt-1 text-sm font-bold text-error">
+                                {errors.instrumentId.message}
+                            </p>
                         )}
-                        <div>
+                        <div className="flex gap-4 flex-wrap mt-2">
                             {instrumentsSelected.map((instrument) => (
-                                <span className="badge badge-soft badge-primary">
-                                    {instrument}
+                                <span
+                                    key={instrument.id}
+                                    className="badge badge-soft badge-primary relative"
+                                >
+                                    {instrument.label}
+                                    <button
+                                        onClick={() =>
+                                            handleDeleteInstrument(instrument)
+                                        }
+                                        type="button"
+                                        className="absolute font-light bg-error bottom-3 -right-1.5 w-4 h-4 rounded-full cursor-pointer text-xs text-base-content hover:scale-110 transition all"
+                                    >
+                                        X
+                                    </button>
                                 </span>
                             ))}
                         </div>
                     </Field>
-                    <span className="text-xs text-base-content/50 italic">
-                        Vous pouvez sélectionner un ou plusieurs instruments
-                    </span>
                 </div>
 
                 {/* form submit */}
@@ -138,6 +203,7 @@ export default function AddMemberToBandDialog({
                         Annuler
                     </Button>
                     <Button
+                        onClick={onSubmit}
                         type="submit"
                         className="flex-1 uppercase italic font-black py-4 sm:py-0"
                     >
