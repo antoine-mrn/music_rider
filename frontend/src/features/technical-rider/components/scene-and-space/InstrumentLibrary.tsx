8e81a504efa49dbbbb622c985e955f3dc3ffff84
antoine-mrn <antoine.marion.am@outlook.fr>
2026-06-11 23:16:44 +0200

    Liste des instruments qui peuvent être placé sur la scène + filtrage de la liste avec l'input de recherche

--- frontend/src/features/technical-rider/components/scene-and-space/InstrumentLibrary.tsx
+++ frontend/src/features/technical-rider/components/scene-and-space/InstrumentLibrary.tsx
@@ -1,3 +1,4 @@
+import { useMemo, useState } from "react";
 import Field from "../../../../components/ui/form/Field";
 import Input from "../../../../components/ui/form/Input";
 import Label from "../../../../components/ui/form/Label";
@@ -10,6 +11,28 @@ interface InstrumentLibraryProps {
 export default function InstrumentLibrary({
     instrumentsList,
 }: InstrumentLibraryProps) {
+    const [instrumentSearch, setInstrumentSearch] = useState("");
+
+    const instrumentSuggestion = useMemo(() => {
+        if (!instrumentSearch) return instrumentsList;
+        const search = instrumentSearch.toLowerCase();
+        return instrumentsList.reduce(
+            (
+                acc: InstrumentCategoryWithInstruments[],
+                item: InstrumentCategoryWithInstruments,
+            ) => {
+                const instrumentFinds = item.Instruments.filter((i) =>
+                    i.label.toLowerCase().includes(search),
+                );
+                if (instrumentFinds.length > 0) {
+                    acc.push({ ...item, Instruments: instrumentFinds });
+                }
+                return acc;
+            },
+            [],
+        );
+    }, [instrumentSearch, instrumentsList]);
+
     return (
         <section className="rounded-lg shadow">
             <div className="bg-base-200 rounded-lg p-4 space-y-2">
@@ -24,9 +47,33 @@ export default function InstrumentLibrary({
                         type="text"
                         id="text"
                         placeholder="Rechercher (ex. Guitare, micro)"
+                        value={instrumentSearch}
+                        onChange={(e) => setInstrumentSearch(e.target.value)}
                     />
                 </Field>
             </div>
+            <ul className="p-4">
+                {instrumentSuggestion.map((instrumentCategory) => (
+                    <li
+                        key={instrumentCategory.id}
+                        className="text-xs font-bold text-base-content/50 uppercase tracking-wider mb-4"
+                    >
+                        {instrumentCategory.label}
+                        <ul className="space-y-1 mt-1">
+                            {instrumentCategory.Instruments.map(
+                                (instrument) => (
+                                    <li
+                                        key={instrument.id}
+                                        className="border border-base-300 normal-case rounded-lg text-center py-1 text-base-content cursor-pointer hover:bg-base-300 hover:border-primary transition-colors"
+                                    >
+                                        {instrument.label}
+                                    </li>
+                                ),
+                            )}
+                        </ul>
+                    </li>
+                ))}
+            </ul>
         </section>
     );
 }
